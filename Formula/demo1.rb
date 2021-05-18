0dd9869e72762f26a6c36edfc40d7f405b49e17f
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-18 01:36:33 -0500

    added sha

--- Formula/demo1.rb
+++ Formula/demo1.rb
@@ -1,33 +1,33 @@
+class Demo1 < Formula
+  version "0.0.0"
+
+  # get the shaval with $: shasum -a 256 <path/to/installfile.tar.gz>
+  sha256 "51d57c363194931a91e6cb8ebc813f8bfa9e6af9d3c36e0c9a6d72504734cb8c"
+  desc "Brew install demo 1"
+  homepage "https://www.TateHanawalt.com"
+  url "https://api.github.com/repos/tatehanawalt/.th_sys/tarball/0.0.0", :using => :curl
+
+  def install
+    lib.install Dir["*"]
+  end
+
+end
+
 
 # -----------------------------------------------------
 # Brewd Builder FormulaMaker
 # @author Tate Hanawalt
 # -----------------------------------------------------
-class Demo1 < Formula
 
-  # STATIC SECTION
-  # ----------------------------------------------------------------------------
-  version "0.0.0"
+# STATIC SECTION
+# ----------------------------------------------------------------------------
 
-  # get the shaval with $: shasum -a 256 <path/to/installfile.tar.gz>
-  sha256 "275ff91052abae98daf4fe730efa81c817f93411af6dac4989e42c107b40e9ab"
-  desc "Brew install demo 1"
-  homepage "https://www.TateHanawalt.com"
-
-  # DOWNLOAD SECTION
-  # ----------------------------------------------------------------------------
-  # TAR Install File Getter
-  # ip = "127.0.0.1"
-  # port = 4123
-  # requrl = "http://#{ip}:#{port}"
-  # url "#{requrl}/linker/darwin/0.0.0", :using => :curl
-  # INSTALL SECTION
-  # ----------------------------------------------------------------------------
-  # def install
-  #   # Brewd .buildconf INSTALLSECTION
-  #   lib.install Dir["*"]
-  #   ENV.deparallelize
-  #   bin.install_symlink lib/"linker.sh" => "linker"
-  # end
-
-end
+# INSTALL SECTION
+# ----------------------------------------------------------------------------
+# def install
+#   # Brewd .buildconf INSTALLSECTION
+#   lib.install Dir["*"]
+#   ENV.deparallelize
+#   bin.install_symlink lib/"linker.sh" => "linker"
+# end
+# tatehanawalt-.th_sys-6d849d2
