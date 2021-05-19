f7f64994e7d4b329625f584b8a2885a10ddcf3fa
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-19 16:41:22 -0500

    everythings fine

---
+++ Formula/demopython.rb
@@ -0,0 +1,45 @@
+#==============================================================================
+#title   :demppython
+#version :0.0.0
+#desc    :python cli built as part of the homebrew demo/dev tools project
+#usage   :See the repo README file for usage
+#exit    :0=success, 1=input error 2=execution error
+#auth    :Dan Henderson(dphender@mtu.edu), Tate Hanawalt(tate@tatehanawalt.com)
+#date    :1621396284
+#==============================================================================
+class Demppython < Formula
+
+  bottle :unneeded                        # formula installed without compilation
+  desc "Brew install demopython"             # formula description
+  homepage "https://www.TateHanawalt.com" # my website
+  revision 0                              # force compile with no version changes
+  version "0.0.0"                         # Formulae version
+
+  head do
+    url "https://github.com/tatehanawalt/.th_sys.git", branch: "main"
+  end
+
+  stable do
+    url "https://github.com/tatehanawalt/.th_sys/releases/download/0.0.0/demopython.tar.gz", :using => :curl
+    sha256 "cbb19c8defe3d76cff134f43abe1385be3d494aa8fa8a6aa5e10fb59288db7f7"
+  end
+
+  def install
+
+    # if build.head?
+    #   cd "demozsh" do
+    #     lib.install ["_demozsh", "demozsh.zsh", "doc/man/demozsh.1"]
+    #   end
+    # else
+    #   lib.install ["_demozsh", "demozsh.zsh", "doc/man/demozsh.1"]
+    # end
+    # zsh_completion.install lib/"_demozsh"
+    # bin.install lib/"demozsh.zsh" => "demozsh"
+    # man1.install lib/"demozsh.1"
+
+
+  end
+
+end
+
+__END__
