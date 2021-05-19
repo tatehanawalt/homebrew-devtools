99a2a58307a00661db8a53a7a1d39fab8aac8174
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-18 21:00:40 -0500

    adding nodejs cli formula and organizing/cleaning up some code

--- Formula/demo1.rb
+++ Formula/demo1.rb
@@ -16,21 +16,27 @@ class Demo1 < Formula
   # ------------------------------------------------------------------------------------------
   # Formulae Methods:
   # ------------------------------------------------------------------------------------------
+
+  # versions download differentiators
+  head do # brew install --HEAD demo1
+    puts "HEAD SECTION:"
+    url "https://github.com/tatehanawalt/.th_sys.git", branch: "main"
+  end
   stable do # brew install demo1
     puts "STABLE SECTION:"
     url "https://github.com/tatehanawalt/.th_sys/releases/download/0.0.1/0.0.1.tar.gz", :using => :curl
     sha256 "c294de88385e86260a6f858219aeb10038e460ebe713f98a44bd5f916b1cf2bf"
   end
-  head do # brew install --HEAD demo1
-    puts "HEAD SECTION:"
-    url "https://github.com/tatehanawalt/.th_sys.git", branch: "main"
-  end
+
+  # os/arch level handlers
   on_linux do
     puts "ON_LINUX SECTION:"
   end
   on_macos do
     puts "ON_MACOS SECTION:"
   end
+
+  # Installatiion Methods
   def install # Install the actual cli regardless of stable or head...
     puts "INSTALL SECTION:"
     cd "demo1" do
@@ -43,6 +49,8 @@ class Demo1 < Formula
   def post_install # Called after installation completes
     puts "POST INSTALL SECTION:"
   end
+
+  # Clean up / Reference
   def caveats # List caveats of this formulae
     <<~EOS
       are there any caveats to brew installs? no... the answer is no.
--- Formula/demonodejs.rb
+++ Formula/demonodejs.rb
@@ -1,29 +1,30 @@
-# demo2                         @author Tate W. Hanawalt               tate@tatehanawalt.com
+# demo1                         @author Tate W. Hanawalt               tate@tatehanawalt.com
 #
-# This is a demo brew formula in the tatehanawalt/devtools homebrew tap for a go based cli
+# this is a demo brew formula in the tatehanawalt/devtools homebrew tap for a zsh based cli
+# with zsh completions
 # ==========================================================================================
-class Demo2 < Formula
+class Demonodejs < Formula
 
   # ------------------------------------------------------------------------------------------
   # Formula vars/params
   # ------------------------------------------------------------------------------------------
-  # bottle :unneeded                       # formula that can be
-  depends_on "go" => :build               # dependencies
-  desc "Brew install demo 2"              # formula description
+  bottle :unneeded                        # formula that can be installed without compilation
+  depends_on "nodejs" => :install         # dependencies
+  desc "Brew install demonodejs"          # formula description
   homepage "https://www.TateHanawalt.com" # my website
   revision 0                              # force compile with no version changes
-  version "0.0.0"                                                  # Formulae version
+  version "0.0.0"                         # Formulae version
 
   # ------------------------------------------------------------------------------------------
   # Formulae Methods:
   # ------------------------------------------------------------------------------------------
 
   # versions download differentiators
-  head do # brew install --HEAD demo2
+  head do # brew install --HEAD demo1
     puts "HEAD SECTION:"
     url "https://github.com/tatehanawalt/.th_sys.git", branch: "main"
   end
-  stable do # brew install demo2
+  stable do # brew install demo1
     puts "STABLE SECTION:"
     url "https://github.com/tatehanawalt/.th_sys/releases/download/0.0.1/0.0.1.tar.gz", :using => :curl
     sha256 "c294de88385e86260a6f858219aeb10038e460ebe713f98a44bd5f916b1cf2bf"
@@ -40,12 +41,11 @@ class Demo2 < Formula
   # Installatiion Methods
   def install # Install the actual cli regardless of stable or head...
     puts "INSTALL SECTION:"
-    cd "demo2" do
-      system "go", "build", "-ldflags", "-s -w -X 'main.Version=#{version}'"
-      lib.install ["demo2", "doc/man/demo2.1"]
+    cd "demonodejs" do
+      lib.install ["main.js", "doc/man/demonodejs.1"]
     end
-    bin.install_symlink lib/"demo2" => "demo2"
-    man1.install lib/"demo2.1"
+    bin.install lib/"main.js" => "demonodejs"
+    man1.install lib/"demonodejs.1"
   end
   def post_install # Called after installation completes
     puts "POST INSTALL SECTION:"
