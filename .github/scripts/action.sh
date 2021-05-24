e37b3e500389c3b3c4d83b1a53a11c1d4760a207
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-23 23:12:18 -0500

    test action

--- .github/scripts/action.sh
+++ .github/scripts/action.sh
@@ -2,13 +2,11 @@
 printf "\n\n"
 
 printf "SH ACTION $0 - ARGS:\n"
-printf " - %s\n" "$@"
+printf "\t- %s\n" "$@"
 printf "\n\n"
 
 printf "ENV:\n"
-env
-printf "\n\n"
-printf " - %s\n" $(env)
+printf "\t- %s\n" $(env)
 printf "\n\n"
 
 exit 0
--- .github/scripts/onmain.sh
+++ .github/scripts/onmain.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 printf "\n\n"
 
-printf "SH ACTION $0 - ARGS:\n"
-printf " - %s\n" "$@"
+printf "SH ONMAIN $0 - ARGS:\n"
+printf "\t- %s\n" "$@"
 printf "\n\n"
 
 printf "ENV:\n"
 env
-# printf " - %s\n"
 printf "\n\n"
 
 exit 0
