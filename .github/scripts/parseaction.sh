42bd5f99b1edaacb7e26d40be36d67aa971d9382
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-24 18:50:14 -0500

    working on action

--- .github/scripts/parseaction.sh
+++ .github/scripts/parseaction.sh
@@ -11,4 +11,20 @@ printf "ENV:\n"
 env | sort
 printf "\n\n"
 
+
+# ENV VARS:
+# GITHUB_ACTIONS = Always set to true when GitHub Actions is running the workflow. You can use this variable to differentiate when tests are being run locally or by GitHub Actions.
+
+
+printf "%-12s%s\n" action "$GITHUB_ACTION"
+printf "%-12s%s\n" trigger "$GITHUB_EVENT_NAME"
+printf "%-12s%s\n" repo "$GITHUB_WORKSPACE"
+printf "%-12s%s\n" commit "$GITHUB_SHA"
+
+
+if [ ! -z "$GITHUB_HEAD_REF" ] && [ ! -z "$GITHUB_REF" ]; then
+  printf "\nPR ACTION\n"
+fi
+
+printf "\n\n"
 exit 0
