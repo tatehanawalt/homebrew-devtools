e1df0c5fef9d1e28cd97e0bff56cfa1597a1918a
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-25 13:57:02 -0500

    added list_labels

--- .github/scripts/inspect_env.sh
+++ .github/scripts/inspect_env.sh
@@ -3,18 +3,44 @@
 # Used as a debugging script to see what an environment looks like
 
 ENV_MAP=$(env)
+MAX_KEY_LEN=0
 
-echo "::group::env"
-printf "ENV:\n"
-printf "\t%s\n" $ENV_MAP | sort -u
+echo "::group::ENV"
+printf "%s\n" "$ENV_MAP"
+echo "::endgroup::"
+IFS="
+"
+ENV_MAP=$(echo "$ENV_MAP" | sort)
+
+echo "::group::ENV_TRAVERSE"
+  for entry in ${ENV_MAP}; do
+    key=$(echo "$entry" | sed 's/=.*//')
+    [ ${#key} -gt $MAX_KEY_LEN ] && MAX_KEY_LEN=${#key}
+    printf "\t%s=%s\n" "$key" $(eval "echo \"\${$key}\"")
+  done
 echo "::endgroup::"
 
-echo "::group::env-traverse"
-for entry in $ENV_MAP; do
-  printf "%s" "$entry"
-done
+# Table output
+echo "::group::ENV_TABLE"
+  for entry in ${ENV_MAP}; do
+    key=$(echo "$entry" | sed 's/=.*//')
+    printf "\t%-${MAX_KEY_LEN}s - %s\n" "$key" $(eval "echo \"\${$key}\"")
+  done
 echo "::endgroup::"
 
-
-printf "LABELS: %s\n" "$LABELS"
-printf "LABELS: %s\n" "${LABELS}"
+if [ ! -z "$INSPECT_ENV_FIELDS" ]; then
+  INSPECT_ENV_FIELDS=$(echo "$INSPECT_ENV_FIELDS" | tr ',' '\n' | sort -u)
+  echo "::group::INSPECT_ENV_FIELDS"
+  echo "INSPECT_ENV:"
+  echo
+  for key in ${INSPECT_ENV_FIELDS}; do
+    printf "\t%s\n" "$key"
+    [ ${#key} -gt $MAX_KEY_LEN ] && MAX_KEY_LEN=${#key}
+  done
+  echo "::endgroup::"
+  echo "::group::INSPECT_ENV_FIELDS_TABLE"
+  for key in ${INSPECT_ENV_FIELDS}; do
+    printf "\t%-${MAX_KEY_LEN}s - %s\n" "$key" $(eval "echo \"\${$key}\"")
+  done
+  echo "::endgroup::"
+fi
