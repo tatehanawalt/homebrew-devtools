b82d9fd7a48b1bf705cb600760171724508cebb5
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-24 19:26:52 -0500

    showing file changes

--- Formula/demozsh.rb
+++ Formula/demozsh.rb
@@ -9,18 +9,22 @@
 # usage   :See the repo README file for usage
 #==============================================================================
 class Demozsh < Formula
-  bottle :unneeded                        # formula that can be installed without compilation
-  desc "Brew install demozsh"             # formula description
-  homepage "https://www.TateHanawalt.com" # my website
-  revision 0                              # force compile with no version changes
-  version "0.0.0"                         # Formulae version
+  desc "Brew install demozsh"
+  homepage "https://www.TateHanawalt.com"
+  version "0.0.0"
+  revision 0
+
+  stable do
+    url "https://github.com/tatehanawalt/th_sys/releases/download/0.0.4/demozsh.tar.gz", using: :curl
+    sha256 "c4d93067c46d0c76a432f7b2d1880310467cc5314bfe694791e845ec2810af4d"
+  end
+
   head do
     url "https://github.com/tatehanawalt/.th_sys.git", branch: "main"
   end
-  stable do
-    url "https://github.com/tatehanawalt/th_sys/releases/download/0.0.4/demozsh.tar.gz", :using => :curl
-    sha256 "c4d93067c46d0c76a432f7b2d1880310467cc5314bfe694791e845ec2810af4d"
-  end
+
+  bottle :unneeded
+
   def install
     if build.head?
       cd "demozsh" do
@@ -33,5 +37,8 @@ class Demozsh < Formula
     bin.install lib/"demozsh.zsh" => "demozsh"
     man1.install lib/"demozsh.1"
   end
+
+  test do
+    assert true
+  end
 end
-__END__
