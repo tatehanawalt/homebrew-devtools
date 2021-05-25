34a89228fe1a92d4715f0a700f04c7d54ed18e31
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-25 01:26:11 -0500

    testing

--- .github/scripts/diff_files.sh
+++ .github/scripts/diff_files.sh
@@ -35,7 +35,7 @@ DIFF_FILES=$(git diff --name-only "$COMPARE_BRANCH")
 printf "DIFF_FILES:\n"
 printf "\t%s\n" $DIFF_FILES | sort -u
 
-echo "::set-output name=action_fruit::strawberry"
+echo "::set-output name=DIFF::strawberry"
 
 exit 0
 
