1f6b6398305b44f7d90f7a217929700831704119
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-25 13:17:01 -0500

    added list_labels

--- .github/scripts/list_labels.sh
+++ .github/scripts/list_labels.sh
@@ -12,40 +12,46 @@ QUERY_BASE=labels
 
 # Aggregate values
 QUERY_URL="https://api.github.com/$QUERY_TOPIC/$QUERY_ORG/$QUERY_REPO/$QUERY_BASE"
-output=$(curl -H "Accept: application/vnd.github.v3+json" $QUERY_URL)
+#output=$(curl -H "Accept: application/vnd.github.v3+json" $QUERY_URL)
 
 # Example output you can use for development and debugging to avoid spamming the
 # from the api:
-#  output='[
-#    {
-#      "id": 3009221173,
-#      "node_id": "MDU6TGFiZWwzMDA5MjIxMTcz",
-#      "url": "https://api.github.com/repos/tatehanawalt/homebrew-devtools/labels/documentation",
-#      "name": "documentation",
-#      "color": "0075ca",
-#      "default": true,
-#      "description": "Improvements or additions to documentation"
-#    },
-#    {
-#      "id": 3009221173,
-#      "node_id": "MDU6TGFiZWwzMDA5MjIxMTcz",
-#      "url": "https://api.github.com/repos/tatehanawalt/homebrew-devtools/labels/documentation",
-#      "name": "sample space",
-#      "color": "0075ca",
-#      "default": true,
-#      "description": "Improvements or additions to documentation"
-#    }
-#  ]'
+output='[
+  {
+    "id": 3009221173,
+    "node_id": "MDU6TGFiZWwzMDA5MjIxMTcz",
+    "url": "https://api.github.com/repos/tatehanawalt/homebrew-devtools/labels/documentation",
+    "name": "documentation",
+    "color": "0075ca",
+    "default": true,
+    "description": "Improvements or additions to documentation"
+  },
+  {
+    "id": 3009221173,
+    "node_id": "MDU6TGFiZWwzMDA5MjIxMTcz",
+    "url": "https://api.github.com/repos/tatehanawalt/homebrew-devtools/labels/documentation",
+    "name": "sample space",
+    "color": "0075ca",
+    "default": true,
+    "description": "Improvements or additions to documentation"
+  }
+]'
 
 echo "OUTPUT:"
 echo "$output"
 
-RESULT=$(echo $output | jq --arg field_name $search_field -r 'map(.[$field_name]) | join(",")')
+RESULT=$(echo $output | jq --arg field_name $search_field -r 'map(.[$field_name]) | join(",")' | sed "s/, /,/g")
 
 echo "RESULT:"
 echo "$RESULT"
+ESCAPED=$(echo $RESULT | sed 's/ /\\ /g')
+echo "$ESCAPED"
+echo "ESCAPED:"
+echo $ESCAPED
+#printf "%q" $ESCAPED
+echo
 
-echo "::set-output name=RESULT::$(printf "%q" $RESULT)"
+echo "::set-output name=RESULT::$ESCAPED"
 
 
 
