75e7f15411d7af5ef01abd5271202fc729a33ec3
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-25 04:24:58 -0500

    test

--- .github/scripts/diff_formula.sh
+++ .github/scripts/diff_formula.sh
@@ -8,20 +8,14 @@ echo "$DIFF_FILES\n"
 formulas=""
 for file in $DIFF_FILES; do
   printf "%s%s\n" "- " "$file"
-  # Match formula files:
-  # if [[ "$file" =~ ^Formula/.*.rb$ ]]; then
-  #   formula=$(echo "$file" | sed 's/^Formula\///' | sed 's/.rb$//')
-  #   echo "formula: $formula"
-  #   formulas="$formulas$formula\n"
-  # fi
 
+  # Match formula files:
   if (echo "$file" | grep -Eq ^Formula/.*.rb$); then
     echo "matched"
     formula=$(echo "$file" | sed 's/^Formula\///' | sed 's/.rb$//')
     echo "formula: $formula"
     formulas="$formulas$formula\n"
   fi
-
 done
 
 formulas=$(echo "$formulas")
@@ -29,5 +23,7 @@ echo "formulas:"
 echo "$formulas"
 
 DIFF_FORMULA="$(printf "$formulas" | tr '\n' ',')"
+
+echo "DIFF_FORMULA=$DIFF_FORMULA"
 
 echo "::set-output name=DIFF_FORMULA::$DIFF_FORMULA"
