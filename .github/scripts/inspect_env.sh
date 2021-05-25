ed74fdf7fbe38902d5457dce37237bffa762b1ca
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-25 14:04:13 -0500

    added list_labels

--- .github/scripts/inspect_env.sh
+++ .github/scripts/inspect_env.sh
@@ -7,36 +7,27 @@
 
 ENV_MAP=$(env)
 MAX_KEY_LEN=0
-
-function log() {
-  echo "::endgroup::"
-  [ ! -z "$1" ] && echo "::group::$1"
-}
-#function egroup() {
-#  echo "::endgroup::"
-#}
-
-log ENV
-printf "%s\n" "$ENV_MAP"
-
 IFS="
 "
 ENV_MAP=$(echo "$ENV_MAP" | sort)
 
-log ENV_TRAVERSE
-for entry in ${ENV_MAP}; do
-  key=$(echo "$entry" | sed 's/=.*//')
-  [ ${#key} -gt $MAX_KEY_LEN ] && MAX_KEY_LEN=${#key}
-  printf "\t%s=%s\n" "$key" $(eval "echo \"\${$key}\"")
-done
+# This function starts a git actions log group. Call with 0 args to end a log
+# group without starting a new one
+function log() {
+  echo "::endgroup::"
+  [ ! -z "$1" ] && echo "::group::$1"
+}
+
+# Log the environment
+log ENV
+printf "%s\n" "$ENV_MAP"
 
 # Table output
 log ENV_TABLE
 for entry in ${ENV_MAP}; do
   key=$(echo "$entry" | sed 's/=.*//')
-  printf "\t%-${MAX_KEY_LEN}s - %s\n" "$key" $(eval "echo \"\${$key}\"")
+  printf "%-${MAX_KEY_LEN}s - %s\n" "$key" $(eval "echo \"\${$key}\"")
 done
-
 
 if [ ! -z "$INSPECT_ENV_FIELDS" ]; then
   INSPECT_ENV_FIELDS=$(echo "$INSPECT_ENV_FIELDS" | tr ',' '\n' | sort -u)
@@ -44,12 +35,12 @@ if [ ! -z "$INSPECT_ENV_FIELDS" ]; then
   log INSPECT_ENV_FIELDS
   for key in ${INSPECT_ENV_FIELDS}; do
     [ ${#key} -gt $MAX_KEY_LEN ] && MAX_KEY_LEN=${#key}
-    printf "\t%s=%s\n" "$key" $(eval "echo \"\${$key}\"")
+    printf "%s=%s\n" "$key" $(eval "echo \"\${$key}\"")
   done
 
   log INSPECT_ENV_FIELDS_TABLE
   for key in ${INSPECT_ENV_FIELDS}; do
-    printf "\t%-${MAX_KEY_LEN}s - %s\n" "$key" $(eval "echo \"\${$key}\"")
+    printf "%-${MAX_KEY_LEN}s - %s\n" "$key" $(eval "echo \"\${$key}\"")
   done
 fi
 
