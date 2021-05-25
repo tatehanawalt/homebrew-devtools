8deb841c55cac20bee8d4f2f5fa0f06d4d3ebd54
Tate <Tate@TateHanawalt.com>
2021-05-25 00:20:22 -0500

    Adding action with linting (#12)
    * Linting action

---
+++ .github/scripts/parseaction.sh
@@ -0,0 +1,153 @@
+#!/bin/sh
+
+COMPARE_BRANCH=dev
+bold_div_line="======================================================================================================="
+div_line="-------------------------------------------------------------------------------------------------------"
+item_prefix="- "
+
+printf "%s\n" "$bold_div_line"
+printf "LINTING RESULTS AT THE BOTTOM OF THE LOG\n"
+printf "%s\n" "$bold_div_line"
+
+if [ -z "$COMPARE_BRANCH" ]; then
+  printf "\$COMPARE_BRANCH length is 0..." 1>&2;
+  exit 2
+fi
+
+# Make sure we are in the github workspace
+if [ -z "$GITHUB_WORKSPACE" ]; then
+  printf "\$GITHUB_WORKSPACE length is 0..." 1>&2;
+  exit 2
+fi
+if [ ! -d "$GITHUB_WORKSPACE" ]; then
+  printf "\$GITHUB_WORKSPACE is not a directory at GITHUB_WORKSPACE=$GITHUB_WORKSPACE" 1>&2;
+  exit 2
+fi
+
+# Takes the FULL filepath of a file to lint
+lint_fs_file () {
+  ext=$(echo "$1" | sed 's/.*\.//g')
+  lint_results=""
+  lint_exit_code=0
+  case "$ext" in
+    gitignore);;
+    md)
+      lint_exit_code=0
+      ;;
+    rb)
+      lint_results=$(rubocop "$1" 2>&1)
+      lint_exit_code=$?
+      if [ $lint_exit_code -ne 0 ]; then
+        printf "\n\tRUBY FAILURE - try running rubocop with --auto-correct\n\n"
+      fi
+      ;;
+    sh);;
+    yml)
+      lint_results=$(ruby -ryaml -e "p YAML.load(STDIN.read)" < $1 2>&1)
+      lint_exit_code=$?
+      [ "$lint_results" = "false" ] && lint_exit_code=2
+      ;;
+    yaml)
+      lint_results=$(ruby -ryaml -e "p YAML.load(STDIN.read)" < $1 2>&1)
+      lint_exit_code=$?
+      [ "$lint_results" = "false" ] && lint_exit_code=2
+      ;;
+    *)
+      lint_results=$(printf "UNHANDLED_EXTENSION=%s\n" "$ext")
+      lint_exit_code=2
+      ;;
+  esac
+  if [ ! -z "$lint_results" ]; then
+    printf "%s\n" "$lint_results" | sed 's/^/\t/'
+  fi
+  return $lint_exit_code
+}
+
+cd $GITHUB_WORKSPACE
+has_dif_branch=$(git branch --list "$COMPARE_BRANCH")
+if [ -z "$has_dif_branch" ]; then
+  git fetch origin $COMPARE_BRANCH
+  git branch dev FETCH_HEAD
+  fetch_exit_code=$?
+  printf "fetch_exit_code=$fetch_exit_code\n"
+fi
+has_dif_branch=$(git branch --list "$COMPARE_BRANCH")
+if [ -z "$has_dif_branch" ]; then
+  printf "\n\nFETCH_HEAD for branch $COMPARE_BRANCH\n" 1>&2;
+  exit 2
+fi
+
+diff_files=$(git diff --name-only "$COMPARE_BRANCH")
+lint_files=""
+lint_extensions=""
+
+for f_path in $diff_files; do
+  full_path="$GITHUB_WORKSPACE/$f_path"
+  if [ -z "$full_path" ]; then
+    printf "full_path length is 0..." 1>&2;
+    exit 2
+  fi
+  if [ -z "$full_path" ]; then
+    continue
+  fi
+  if [ -f "$full_path" ]; then
+    lint_files="$lint_files$full_path\n"
+    modified_ext=$(echo "$full_path" | sed 's/.*\.//g')
+    if [ ! -z "$modified_ext" ]; then
+      lint_extensions="$lint_extensions$modified_ext\n"
+    fi
+  fi
+done
+
+IFS="
+"
+# This part just logs the linting set
+lint_files=$(echo "$lint_files" | sort -u )
+printf "FILES:\n"
+for f_path in $lint_files; do
+  printf "%s%s\n" "$item_prefix" "$f_path"
+done
+lint_extensions=$(echo "$lint_extensions" | sort -u )
+printf "EXTENSIONS:\n"
+for ext in $lint_extensions; do
+  printf "%s%s\n" "$item_prefix" "$ext"
+done
+
+for ext in $lint_extensions; do
+  case "$ext" in
+    rb)
+      printf "Will lint ruby\n"
+      printf "\tRUBY:    %s\n" "$(rubocop --version)"
+      printf "\tRUBOCOP: %s\n" "$(ruby --version)"
+      ;;
+  esac
+done
+
+
+# Actually lint the files
+lint_failures=""
+for ext in $lint_extensions; do
+  printf "%s\n" "$div_line"
+  printf "EXT=$ext\n"
+  lint_set=$(echo "$lint_files" | grep "$ext\$" | sort -u)
+  for lint_file in $lint_set; do
+    printf "%sFILE=%s\n" "$item_prefix" "$lint_file"
+    lint_fs_file $lint_file
+    if [ $? -ne 0 ]; then
+      lint_failures="$lint_failures$lint_file\n"
+    fi
+  done
+done
+
+if [ ! -z "$lint_failures" ]; then
+  printf "%s\n" "$bold_div_line"
+  printf "LINTING=failed\n"
+  printf "FILES:\n"
+  lint_failures=$(echo "$lint_failures" | sort -u)
+  for failed_lint in $lint_failures; do
+    printf "%s%s\n" "$item_prefix" "$failed_lint"
+  done
+  printf "%s\n" "$bold_div_line"
+  exit 1
+fi
+exit 0
--- Formula/demozsh.rb
+++ Formula/demozsh.rb
@@ -1,37 +1,47 @@
-#==============================================================================
+# ==============================================================================
 # title   :demozsh
 # version :0.0.0
 # desc    :zsh cli built as part of the homebrew demo/dev tools project
 # auth    :Tate Hanawalt(tate@tatehanawalt.com)
 # date    :1621396284
-#==============================================================================
+# ==============================================================================
 # exit    :0=success, 1=input error 2=execution error
 # usage   :See the repo README file for usage
-#==============================================================================
+# ==============================================================================
+# frozen_string_literal: true
+
+# Demozsh Formula
 class Demozsh < Formula
-  bottle :unneeded                        # formula that can be installed without compilation
-  desc "Brew install demozsh"             # formula description
-  homepage "https://www.TateHanawalt.com" # my website
-  revision 0                              # force compile with no version changes
-  version "0.0.0"                         # Formulae version
-  head do
-    url "https://github.com/tatehanawalt/.th_sys.git", branch: "main"
-  end
+  desc 'Brew install demozsh'
+  homepage 'https://www.TateHanawalt.com'
+  version '0.0.0'
+  revision 0
+
   stable do
-    url "https://github.com/tatehanawalt/th_sys/releases/download/0.0.4/demozsh.tar.gz", :using => :curl
-    sha256 "c4d93067c46d0c76a432f7b2d1880310467cc5314bfe694791e845ec2810af4d"
+    url 'https://github.com/tatehanawalt/th_sys/releases/download/0.0.4/demozsh.tar.gz', using: :curl
+    sha256 'c4d93067c46d0c76a432f7b2d1880310467cc5314bfe694791e845ec2810af4d'
   end
-  def install
+
+  head do
+    url 'https://github.com/tatehanawalt/.th_sys.git', branch: 'main'
+  end
+
+  bottle :unneeded
+
+  install do
     if build.head?
-      cd "demozsh" do
-        lib.install ["_demozsh", "demozsh.zsh", "doc/man/demozsh.1"]
+      cd 'demozsh' do
+        lib.install ['_demozsh', 'demozsh.zsh', 'doc/man/demozsh.1']
       end
     else
-      lib.install ["_demozsh", "demozsh.zsh", "doc/man/demozsh.1"]
+      lib.install ['_demozsh', 'demozsh.zsh', 'doc/man/demozsh.1']
     end
-    zsh_completion.install lib/"_demozsh"
-    bin.install lib/"demozsh.zsh" => "demozsh"
-    man1.install lib/"demozsh.1"
+    zsh_completion.install "#{lib}_demozsh"
+    bin.install lib / 'demozsh.zsh' => 'demozsh'
+    man1.install lib / 'demozsh.1'
+  end
+
+  test do
+    assert true
   end
 end
-__END__
