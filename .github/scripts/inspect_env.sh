48d9489b48959501dcc7e3e0bb6cd867ad7fabd9
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-25 14:27:21 -0500

    simplified inspect_env.sh

--- .github/scripts/inspect_env.sh
+++ .github/scripts/inspect_env.sh
@@ -5,48 +5,37 @@
 # Specify INSPECT_ENV_FIELDS=<field>,<field2>... to see a list of specific
 # fields
 
-ENV_MAP=$(env)
-MAX_KEY_LEN=0
-IFS="
-"
-ENV_MAP=$(echo "$ENV_MAP" | sort)
-
 # This function starts a git actions log group. Call with 0 args to end a log
 # group without starting a new one
-log () {
-  echo "::endgroup::"
-  [ ! -z "$1" ] && echo "::group::$1"
+log() { echo "::endgroup::"; [ ! -z "$1" ] && echo "::group::$1"; }
+
+# Pass this function the set of comma-separated keys to inspect the environment
+# variable value of each key
+inspect_fields() {
+  log $1
+  shift
+  fields=$(printf "%s" "$1" | \
+    sed 's/^,//' | \
+    sed 's/,$//' | \
+    tr ',' '\n' | \
+    sort -u | \
+    sed '/^[[:space:]]*$/d')
+
+  max_field_len=0
+  for key in ${fields}; do
+    [ ${#key} -gt $max_field_len ] && max_field_len=${#key}
+    printf "%s=%s\n" $key $(eval "echo \"\$$key\"")
+  done
+  log "$1_TABLE"
+  for key in ${fields}; do
+    printf "%-${max_field_len}s - %s\n" $key $(eval "echo \"\$$key\"")
+  done
+  log
 }
 
-# Log the environment
-log ENV
-printf "%s\n" "$ENV_MAP"
-
-# Table output
-log ENV_TABLE
-for entry in ${ENV_MAP}; do
-  key=$(echo "$entry" | sed 's/=.*//')
-  [ ${#key} -gt $MAX_KEY_LEN ] && MAX_KEY_LEN=${#key}
-done
-for entry in ${ENV_MAP}; do
-  key=$(echo "$entry" | sed 's/=.*//')
-  printf "%-${MAX_KEY_LEN}s - %s\n" "$key" $(eval "echo \"\${$key}\"")
-done
-
-if [ ! -z "$INSPECT_ENV_FIELDS" ]; then
-  INSPECT_ENV_FIELDS=$(echo "$INSPECT_ENV_FIELDS" | tr ',' '\n' | sort -u)
-  log INSPECT_ENV_FIELDS
-  for key in ${INSPECT_ENV_FIELDS}; do
-    [ ${#key} -gt $MAX_KEY_LEN ] && MAX_KEY_LEN=${#key}
-    printf "%s=%s\n" "$key" $(eval "echo \"\${$key}\"")
-  done
-
-  log INSPECT_ENV_FIELDS_TABLE
-  for key in ${INSPECT_ENV_FIELDS}; do
-    printf "%-${MAX_KEY_LEN}s - %s\n" "$key" $(eval "echo \"\${$key}\"")
-  done
-fi
-
-log
-
+# DO NOT MODIFY IFS!
+IFS="
+"
+inspect_fields ENV $(printf "%s" "$(env)" | sed 's/=.*//g' | tr '\n' ',')
+[ ! -z "$INSPECT_ENV_FIELDS" ] && inspect_fields INSPECT_ENV_FIELDS $INSPECT_ENV_FIELDS
 exit 0
