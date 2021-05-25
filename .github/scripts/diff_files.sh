c4f4e389954357fa75c6bd28664d147f5a54996b
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-25 00:54:29 -0500

    removed default COMPARE_BRANCH

--- .github/scripts/diff_files.sh
+++ .github/scripts/diff_files.sh
@@ -1,38 +1,34 @@
 #!/bin/sh
-COMPARE_BRANCH=dev
 
+# COMPARE_BRANCH=dev
 if [ -z "$COMPARE_BRANCH" ]; then
-  printf "\$COMPARE_BRANCH length is 0..." 1>&2;
+  printf "COMPARE_BRANCH length is 0... set COMPARE_BRANCH=<branch_name>\n" 1>&2;
   exit 2
 fi
-
 # Make sure we are in the github workspace
 if [ -z "$GITHUB_WORKSPACE" ]; then
-  printf "\$GITHUB_WORKSPACE length is 0..." 1>&2;
+  printf "\$GITHUB_WORKSPACE length is 0...\n" 1>&2;
   exit 2
 fi
-
 if [ ! -d "$GITHUB_WORKSPACE" ]; then
-  printf "\$GITHUB_WORKSPACE is not a directory at GITHUB_WORKSPACE=$GITHUB_WORKSPACE" 1>&2;
+  printf "\$GITHUB_WORKSPACE is not a directory at GITHUB_WORKSPACE=$GITHUB_WORKSPACE\n" 1>&2;
   exit 2
 fi
-
-printf "\n\nCOMPARE_BRANCH=%s\n\n\n" "$COMPARE_BRANCH"
-
+printf "COMPARE_BRANCH=%s\n" "$COMPARE_BRANCH"
 cd $GITHUB_WORKSPACE
 has_dif_branch=$(git branch --list "$COMPARE_BRANCH")
 if [ -z "$has_dif_branch" ]; then
-  git fetch origin $COMPARE_BRANCH
-  git branch dev FETCH_HEAD
+  git fetch origin "$COMPARE_BRANCH" &>/dev/null
+  git branch "$COMPARE_BRANCH" FETCH_HEAD &>/dev/null
   fetch_exit_code=$?
-  printf "fetch_exit_code=$fetch_exit_code\n"
+  printf "FETCH_EXIT_CODE=$fetch_exit_code\n"
 fi
 has_dif_branch=$(git branch --list "$COMPARE_BRANCH")
 if [ -z "$has_dif_branch" ]; then
   printf "\n\nFETCH_HEAD for branch $COMPARE_BRANCH\n" 1>&2;
   exit 2
 fi
-diff_files=$(git diff --name-only "$COMPARE_BRANCH")
-for f_path in $diff_files; do
+DIFF_FILES=$(git diff --name-only "$COMPARE_BRANCH")
+for f_path in $DIFF_FILES; do
   printf "%s\n" "$f_path"
 done
