7d5333d4633a67285b3dae9c35ff1e6fba1f6bce
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-24 20:59:45 -0500

    moved yml to yaml for testing

--- .github/scripts/parseaction.sh
+++ .github/scripts/parseaction.sh
@@ -1,66 +1,103 @@
 #!/bin/sh
-printf "\n\n"
-
 COMPARE_BRANCH=dev
-
-
-printf "SH ONMAIN $0 - ARGS:\n"
-if [ ${#@} -gt 0 ]; then
-  printf "\t- %s\n" "$@"
-  printf "\n"
-fi
-printf "\n"
-printf "ENV:\n"
-env | sort
-printf "\n\n"
-
-# ENV VARS:
-# GITHUB_ACTIONS = Always set to true when GitHub Actions is running the workflow. You can use this variable to differentiate when tests are being run locally or by GitHub Actions.
-
-printf "%-12s%s\n" action "$GITHUB_ACTION"
-printf "%-12s%s\n" trigger "$GITHUB_EVENT_NAME"
-printf "%-12s%s\n" repo "$GITHUB_WORKSPACE"
-printf "%-12s%s\n" commit "$GITHUB_SHA"
-
-if [ ! -z "$GITHUB_HEAD_REF" ] && [ ! -z "$GITHUB_REF" ]; then
-  printf "\nPR ACTION\n"
+if [ -z "$COMPARE_BRANCH" ]; then
+  printf "\$COMPARE_BRANCH length is 0..." 1>&2;
+  exit 2
 fi
 
-printf "\n\n"
+# Make sure we are in the github workspace
+if [ -z "$GITHUB_WORKSPACE" ]; then
+  printf "\$GITHUB_WORKSPACE length is 0..." 1>&2;
+  exit 2
+fi
+if [ ! -d "$GITHUB_WORKSPACE" ]; then
+  printf "\$GITHUB_WORKSPACE is not a directory at GITHUB_WORKSPACE=$GITHUB_WORKSPACE" 1>&2;
+  exit 2
+fi
 
 cd $GITHUB_WORKSPACE
-git fetch origin dev
-git branch dev FETCH_HEAD
-diff_files=$(git diff --name-only dev)
+has_dif_branch=$(git branch --list "$COMPARE_BRANCH")
+if [ -z "$has_dif_branch" ]; then
+  printf "\n\nFETCHING COMPARE BRANCH: $COMPARE_BRANCH\n\n"
+  git fetch origin $COMPARE_BRANCH
+  git branch dev FETCH_HEAD
+fi
+has_dif_branch=$(git branch --list "$COMPARE_BRANCH")
+if [ -z "$has_dif_branch" ]; then
+  printf "\n\nFETCH_HEAD for branch $COMPARE_BRANCH\n" 1>&2;
+  exit 2
+fi
 
-printf "\n\nDIFF_FILES:\n"
-printf " - %s\n" $diff_files
-printf "\n\n"
+diff_files=$(git diff --name-only "$COMPARE_BRANCH")
 
-declare a lint_files
+lint_files=()
+lint_extensions=()
 
 for f_path in $diff_files; do
   full_path="$GITHUB_WORKSPACE/$f_path"
-
-  printf "\tDIFF PATH: %s\n" $full_path
-
   if [ -z "$full_path" ]; then
     printf "full_path length is 0..." 1>&2;
-    lint_files+=($full_path)
-    return 2
+    exit 2
   fi
-
+  if [ -z "$full_path" ]; then
+    continue
+  fi
   if [ -f "$full_path" ]; then
-    modified_ext=$(echo $full_path | sed 's/.*\.//g')
-    printf "Modified Extension: $modified_ext\n"
-  else
-    # File was removed...
+    lint_files="$lint_files$full_path\n"
+    modified_ext=$(echo "$full_path" | sed 's/.*\.//g')
+    if [ ! -z "$modified_ext" ]; then
+      lint_extensions="$lint_extensions$modified_ext\n"
+    fi
   fi
 done
 
+lint_files=($(printf "$lint_files" | sort -u))
+printf "LINT FILES: %d\n" "${#lint_files[@]}"
+for lint_file in ${lint_files[@]}; do
+  echo "\t$lint_file"
+done
+
+lint_extensions=($(printf "$lint_extensions" | sort -u))
+printf "LINT EXTENSIONS: %d\n" ${#lint_extensions[@]}
+for ext in ${lint_extensions[@]}; do
+  echo "\t$ext"
+
+  case "$ext" in
+    md)
+
+      ;;
+    rb)
+      ;;
+    sh)
+      ;;
+    yml|yaml)
+      printf "\nYAML!\n"
+      ;;
+  esac
 
 
-printf "\n\nLINT FILES:\n"
-printf " - %s\n" $lint_files
+done
 
 exit 0
+
+
+
+
+# printf "SH ONMAIN $0 - ARGS:\n"
+# if [ ${#@} -gt 0 ]; then
+#   printf "\t- %s\n" "$@"
+#   printf "\n"
+# fi
+# printf "\n"
+# printf "ENV:\n"
+# env | sort
+# printf "\n\n"
+# # ENV VARS:
+# # GITHUB_ACTIONS = Always set to true when GitHub Actions is running the workflow. You can use this variable to differentiate when tests are being run locally or by GitHub Actions.
+# printf "%-12s%s\n" action "$GITHUB_ACTION"
+# printf "%-12s%s\n" trigger "$GITHUB_EVENT_NAME"
+# printf "%-12s%s\n" repo "$GITHUB_WORKSPACE"
+# printf "%-12s%s\n" commit "$GITHUB_SHA"
+# if [ ! -z "$GITHUB_HEAD_REF" ] && [ ! -z "$GITHUB_REF" ]; then
+#   printf "\nPR ACTION\n"
+# fi
