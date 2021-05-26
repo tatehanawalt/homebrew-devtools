599eff673dd040a93d7eb2b6d8b9d686e49f0a09
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-26 14:05:04 -0500

    update

--- .github/scripts/diff_files.sh
+++ .github/scripts/diff_files.sh
@@ -35,13 +35,16 @@ write_result_set() {
 
 #  Compare against the main branch
 [ -z "$GITHUB_BASE_REF" ] && GITHUB_BASE_REF=main
-
-# Using the HEAD branch
 [ -z "$GITHUB_HEAD_REF" ] && GITHUB_HEAD_REF=main
-if [ -z "$GITHUB_HEAD_REF" ]; then
-  echo "GITHUB_HEAD_REF length is 0... set GITHUB_HEAD_REF=<branch_name>"
+
+
+# Using the BASE branch
+if [ -z "$GITHUB_BASE_REF" ]; then
+  echo "GITHUB_BASE_REF length is 0... set GITHUB_BASE_REF=<branch_name>"
   exit 2
 fi
+echo "GITHUB_BASE_REF=$GITHUB_BASE_REF"
+
 if [ -z "$GITHUB_WORKSPACE" ]; then
   echo "GITHUB_WORKSPACE length is 0"
   exit 2
@@ -50,9 +53,9 @@ if [ ! -d "$GITHUB_WORKSPACE" ]; then
   echo "GITHUB_WORKSPACE is not a directory at GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
   exit 2
 fi
-
-echo "GITHUB_HEAD_REF=$BASE"
 cd $GITHUB_WORKSPACE
+
+
 has_dif_branch=$(git branch --list "$GITHUB_BASE_REF")
 if [ -z "$has_dif_branch" ]; then
   git fetch origin "$GITHUB_BASE_REF"
@@ -60,6 +63,7 @@ if [ -z "$has_dif_branch" ]; then
   fetch_exit_code=$?
   echo "FETCH_EXIT_CODE=$fetch_exit_code"
 fi
+
 has_dif_branch=$(git branch --list "$GITHUB_BASE_REF")
 if [ -z "$has_dif_branch" ]; then
   echo "FETCH_HEAD for branch $GITHUB_BASE_REF"
@@ -71,6 +75,8 @@ fi
 DIFF_FILES=$(git diff --name-only "$GITHUB_BASE_REF")
 printf "DIFF_FILES:\n"
 printf "\t%s\n" $DIFF_FILES | sort -u
+
+
 DIFF_DIRS=""
 for file_path in $DIFF_FILES; do
   dir=$(dirname $file_path)
@@ -94,6 +100,7 @@ DIFF_DIRS=$(echo $DIFF_DIRS | sed 's/^ //g' | \
   sed 's/ /,/g')
 
 echo "$DIFF_DIRS"
+
 echo "::set-output name=DIFF_BRANCH::$GITHUB_HEAD_REF"
 echo "::set-output name=DIFF_FILES::$DIFF_FILES"
 echo "::set-output name=DIFF_DIRS::$DIFF_DIRS"
