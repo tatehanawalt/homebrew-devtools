ea8de3e0f25a6eab4eb712208cbe495d1ffcceec
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-26 16:50:57 -0500

    update

--- .github/scripts/label_pr.sh
+++ .github/scripts/label_pr.sh
@@ -14,10 +14,24 @@ done
 printf "\n"
 
 
-# curl \
-#   -X POST \
-#   -H "Accept: application/vnd.github.v3+json" \
-#   https://api.github.com/repos/octocat/hello-world/issues/42/labels
+[ -z "$GITHUB_API_URL" ]          && GITHUB_API_URL="https://api.github.com"
+[ -z "$GITHUB_BASE_REF" ]         && GITHUB_BASE_REF="main"
+[ -z "$GITHUB_HEAD_REF" ]         && GITHUB_HEAD_REF="main"
+[ -z "$GITHUB_REPOSITORY" ]       && GITHUB_REPOSITORY="tatehanawalt/homebrew-devtools"
+[ -z "$GITHUB_REPOSITORY_OWNER" ] && GITHUB_REPOSITORY_OWNER="tatehanawalt"
+[ -z "$GITHUB_WORKSPACE" ]        && GITHUB_WORKSPACE=$(git rev-parse --show-toplevel)
+OWNER="$GITHUB_REPOSITORY_OWNER"
+REPO=$(echo "$GITHUB_REPOSITORY" | sed 's/.*\///')
+
+REQUEST_URL="https://api.github.com/repos/$OWNER/$REPO/issues/$ID/labels"
+
+printf "\nREQUEST_URL=%s\n" "$REQUEST_URL"
+
+curl \
+  -X POST \
+  -d '["devenv"]' \
+  -H "Accept: application/vnd.github.v3+json" \
+  "$REQUEST_URL"
 
 
 printf "\n"
