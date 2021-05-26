e0295cdb3e2545ab09e9d522c0d0dbda5cc58bee
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-26 16:44:46 -0500

    update

--- .github/scripts/label_pr.sh
+++ .github/scripts/label_pr.sh
@@ -5,4 +5,19 @@ printf "\n\nLABEL PR\n\n"
 printf "ID=%s\n" "$ID"
 printf "LABELS=%s\n" "$LABELS"
 
-printf "\n\n"
+labels=$($(echo -e "${LABELS[@]}" | tr ',' '\n'))
+
+printf "labels:\n"
+for label in ${labels[@]}; do
+  printf "\t%s\n" "$label"
+done
+printf "\n"
+
+
+# curl \
+#   -X POST \
+#   -H "Accept: application/vnd.github.v3+json" \
+#   https://api.github.com/repos/octocat/hello-world/issues/42/labels
+
+
+printf "\n"
