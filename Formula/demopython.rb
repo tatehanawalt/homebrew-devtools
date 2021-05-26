29b9e927ab1839b20df989801140e15d8ead06c5
Tate <Tate@TateHanawalt.com>
2021-05-26 17:07:51 -0500

    update (#15)
    demo python label automatically applied

--- Formula/demopython.rb
+++ Formula/demopython.rb
@@ -13,26 +13,26 @@
 # Demopython Formula - brew formula for an example python script
 class Demopython < Formula
   bottle :unneeded                        # formula installed without compilation
-  desc "Brew install demopython"          # formula description
-  homepage "https://www.TateHanawalt.com" # my website
+  desc 'Brew install demopython'          # formula description
+  homepage 'https://www.TateHanawalt.com' # my website
   revision 0                              # force compile with no version changes
-  version "0.0.0"                         # Formulae version
+  version '0.0.0'                         # Formulae version
   head do
-    url "https://github.com/tatehanawalt/.th_sys.git", branch: "main"
+    url 'https://github.com/tatehanawalt/.th_sys.git', branch: 'main'
   end
   stable do
-    url "https://github.com/tatehanawalt/th_sys/releases/download/0.0.4/demopython.tar.gz", :using => :curl
-    sha256 "2303c97efa30a663302153ff8c69e40cfbed7d5d86a8d7272580e105df1a3897"
+    url 'https://github.com/tatehanawalt/th_sys/releases/download/0.0.4/demopython.tar.gz', :using => :curl
+    sha256 '2303c97efa30a663302153ff8c69e40cfbed7d5d86a8d7272580e105df1a3897'
   end
   def install
     if build.head?
-      cd "demopython" do
-        lib.install ["main.py", "doc/man/demopython.1"]
+      cd 'demopython' do
+        lib.install ['main.py', 'doc/man/demopython.1']
       end
     else
-      lib.install ["main.py", "doc/man/demopython.1"]
+      lib.install ['main.py", "doc/man/demopython.1']
     end
-    bin.install lib/"main.py" => "demopython"
-    man1.install lib/"demopython.1"
+    bin.install lib/'main.py' => 'demopython'
+    man1.install lib/'demopython.1'
   end
 end
