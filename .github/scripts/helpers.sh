cb5c9c2770fcfee48f01dfc82980a15ab9ecb9d9
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-26 19:50:45 -0500

    update

--- .github/scripts/helpers.sh
+++ .github/scripts/helpers.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+
+
+
 helper1() {
 
   printf "\n\nTHIS IS THE HELPER 1 SCRIPT!\n\n"
--- .github/scripts/testing.sh
+++ .github/scripts/testing.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-source $GITHUB_WORKSPACE/.github/scripts/helpers.sh
+printf "\n\nGITHUB_WORKSPACE=$GITHUB_WORKSPACE\n\n"
+
+
+
+source "$GITHUB_WORKSPACE/.github/scripts/helpers.sh"
 
 helper1
 
