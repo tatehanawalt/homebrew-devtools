1a2f9a7b9b3a87edbc4dcd9c1a61145a1f79592e
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-26 20:00:26 -0500

    update

--- .github/scripts/testing.sh
+++ .github/scripts/testing.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+source "$GITHUB_WORKSPACE/.github/scripts/helpers.sh"
+
+
+
 printf "\n\nGITHUB_WORKSPACE=$GITHUB_WORKSPACE\n\n"
-
-
-
 printf "pwd: %s\n" $(pwd)
-
-# source "$GITHUB_WORKSPACE/.github/scripts/helpers.sh"
 # source "$GITHUB_WORKSPACE/.github/scripts/helpers.sh"
 
 helper1
