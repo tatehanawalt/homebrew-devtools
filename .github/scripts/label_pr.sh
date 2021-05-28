804cb17acc850374071b0d00a15160a2ab7ea0b4
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-28 12:29:58 -0500

    update

--- .github/scripts/label_pr.sh
+++ .github/scripts/label_pr.sh
@@ -6,7 +6,7 @@ labels_csv=${LABELS[@]}
 labels=($(echo -e "${LABELS[@]}" | tr ',' '\n'))
 write_result_set "$labels_csv" LABEL_PR_LABELS
 
-post_body=$(printf "[%s]" $(printf "\"%s\"," "${labels[@]}" | sed 's/,$//'))
+data=$(printf "[%s]" $(printf "\"%s\"," "${labels[@]}" | sed 's/,$//'))
 [ -z "$GITHUB_API_URL" ]          && GITHUB_API_URL="https://api.github.com"
 [ -z "$GITHUB_BASE_REF" ]         && GITHUB_BASE_REF="main"
 [ -z "$GITHUB_HEAD_REF" ]         && GITHUB_HEAD_REF="main"
@@ -19,17 +19,17 @@ REQUEST_URL="https://api.github.com/repos/$OWNER/$REPO/issues/$ID/labels"
 
 printf "ID=%s\n" "$ID"
 printf "LABELS=%s\n" "$LABELS"
-printf "post_body: %s\n" "$post_body"
-printf "\nREQUEST_URL=%s\n" "$REQUEST_URL"
+printf "data: %s\n" "$data"
+printf "REQUEST_URL=%s\n" "$REQUEST_URL"
 
-curl \
+response=$(curl \
   -X POST \
-  -d $post_body \
+  -s \
+  -w "HTTPSTATUS:%{http_code}" \
   -H "Authorization: token $GITHUB_AUTH_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
-  "$REQUEST_URL"
-
-printf "\n"
+  "$REQUEST_URL" \
+  -d "$data" )
 
 before_exit
 exit 0
