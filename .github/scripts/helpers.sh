1f2c1e6e7913299edfddfbbd41ab86cd342ce9fa
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-28 12:32:51 -0500

    update

--- .github/scripts/helpers.sh
+++ .github/scripts/helpers.sh
@@ -158,6 +158,8 @@ before_exit() {
   write_result_set "$(join_by , ${HELPERS_LOG_TOPICS[@]})" outputs
   log
 }
+
+
 create_label() {
   label="$1"
   color="$2"
@@ -186,6 +188,44 @@ create_label() {
     '. += {"response_code":$response_code} | . += {"request_url":$request_url} | tostring'
   return $request_status
 }
+
+label_pr() {
+
+  printf "LABEL_PR Helper!\n"
+
+  printf "%s\n" ${@}
+
+  return 0
+
+  labels_csv=${LABELS[@]}
+  labels=($(echo -e "${LABELS[@]}" | tr ',' '\n'))
+  write_result_set "$labels_csv" LABEL_PR_LABELS
+  data=$(printf "[%s]" $(printf "\"%s\"," "${labels[@]}" | sed 's/,$//'))
+  [ -z "$GITHUB_API_URL" ]          && GITHUB_API_URL="https://api.github.com"
+  [ -z "$GITHUB_BASE_REF" ]         && GITHUB_BASE_REF="main"
+  [ -z "$GITHUB_HEAD_REF" ]         && GITHUB_HEAD_REF="main"
+  [ -z "$GITHUB_REPOSITORY" ]       && GITHUB_REPOSITORY="tatehanawalt/homebrew-devtools"
+  [ -z "$GITHUB_REPOSITORY_OWNER" ] && GITHUB_REPOSITORY_OWNER="tatehanawalt"
+  [ -z "$GITHUB_WORKSPACE" ]        && GITHUB_WORKSPACE=$(git rev-parse --show-toplevel)
+  OWNER="$GITHUB_REPOSITORY_OWNER"
+  REPO=$(echo "$GITHUB_REPOSITORY" | sed 's/.*\///')
+  REQUEST_URL="https://api.github.com/repos/$OWNER/$REPO/issues/$ID/labels"
+  printf "ID=%s\n" "$ID"
+  printf "LABELS=%s\n" "$LABELS"
+  printf "data: %s\n" "$data"
+  printf "REQUEST_URL=%s\n" "$REQUEST_URL"
+  response=$(curl \
+    -X POST \
+    -s \
+    -w "HTTPSTATUS:%{http_code}" \
+    -H "Authorization: token $GITHUB_AUTH_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    "$REQUEST_URL" \
+    -d "$data" )
+  before_exit
+  exit 0
+}
+
 
 # log $key
 # printf "$key=$result\n"
