4f16d04dda6e0a01c5c7eeb165988382fccfed26
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-28 12:24:08 -0500

    update

--- .github/scripts/label_pr.sh
+++ .github/scripts/label_pr.sh
@@ -6,18 +6,7 @@ labels_csv=${LABELS[@]}
 labels=($(echo -e "${LABELS[@]}" | tr ',' '\n'))
 write_result_set "$labels_csv" LABEL_PR_LABELS
 
-
-
 post_body=$(printf "[%s]" $(printf "\"%s\"," "${labels[@]}" | sed 's/,$//'))
-
-
-printf "ID=%s\n" "$ID"
-printf "LABELS=%s\n" "$LABELS"
-printf "post_body: %s\n" "$post_body"
-
-
-exit 0
-
 [ -z "$GITHUB_API_URL" ]          && GITHUB_API_URL="https://api.github.com"
 [ -z "$GITHUB_BASE_REF" ]         && GITHUB_BASE_REF="main"
 [ -z "$GITHUB_HEAD_REF" ]         && GITHUB_HEAD_REF="main"
@@ -26,9 +15,11 @@ exit 0
 [ -z "$GITHUB_WORKSPACE" ]        && GITHUB_WORKSPACE=$(git rev-parse --show-toplevel)
 OWNER="$GITHUB_REPOSITORY_OWNER"
 REPO=$(echo "$GITHUB_REPOSITORY" | sed 's/.*\///')
-
 REQUEST_URL="https://api.github.com/repos/$OWNER/$REPO/issues/$ID/labels"
 
+printf "ID=%s\n" "$ID"
+printf "LABELS=%s\n" "$LABELS"
+printf "post_body: %s\n" "$post_body"
 printf "\nREQUEST_URL=%s\n" "$REQUEST_URL"
 
 curl \
@@ -40,4 +31,5 @@ curl \
 
 printf "\n"
 
-# post_body=$(printf "[%s]" "$post_body")
+before_exit
+exit 0
