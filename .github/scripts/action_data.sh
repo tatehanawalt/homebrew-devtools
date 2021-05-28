bf8a703d4409ed1bd7224a9d3a17efa95635ed97
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-28 11:30:35 -0500

    update

--- .github/scripts/action_data.sh
+++ .github/scripts/action_data.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
-. "$(dirname $0)/helpers.sh" $@
+. "$(dirname $0)/helpers.sh"
 
 if [ ! -f "$GITHUB_EVENT_PATH" ]; then
   write_error "$(basename $0) GITHUB_EVENT_PATH file not found at $GITHUB_EVENT_PATH - line $LINENO"
   exit 1
 fi
 
-log EVENT_FILE
-cat $GITHUB_EVENT_PATH | jq 'keys'
-
-exit
-
-log EVENT_$(echo $GITHUB_EVENT_NAME | tr [:lower:] [:upper:])
+event_file_attributes=($(cat $GITHUB_EVENT_PATH | jq -r 'keys | join("\n")'))
+event_file_attributes_csv=$(join_by , ${event_file_attributes[@]})
+event_file=$(cat $GITHUB_EVENT_PATH | jq -r 'keys | join("\n")')
 REPOSITORY_JSON=$(cat $GITHUB_EVENT_PATH | jq '.repository')
 REPOSITORY_ID=$(echo "$REPOSITORY_JSON" | jq -r '.id')
-write_result_set "$REPOSITORY_ID" "REPOSITORY_ID"
 REPO=$(echo "$REPOSITORY_JSON" | jq -r '.name')
+
+log EVENT_$(echo $GITHUB_EVENT_NAME | tr [:lower:] [:upper:])
+
+write_result_set "$event_file_attributes_csv" event_file_attributes
+write_result_set "$REPOSITORY_ID" "REPOSITORY_ID"
 write_result_set "$REPO" "REPO"
 
 case $GITHUB_EVENT_NAME in
   pull_request)
-    # log PULL_REQUEST
-    # AFTER -> NEW COMMIT
-    # BEFORE -> OLD COMMIT
-    # printf "GITHUB_REF=%s\n" "$GITHUB_REF"
     PULL_REQUEST_JSON=$(cat $GITHUB_EVENT_PATH | jq '.pull_request')
     ID=$(cat $GITHUB_EVENT_PATH | jq '.number')
     write_result_set "$ID" ID
@@ -35,20 +32,6 @@ case $GITHUB_EVENT_NAME in
     ;;
   push)
     printf "PUSH:\n"
-    #printf "\n\nPUSH\n\n"
-    # "after",
-    # "base_ref",
-    # "before",
-    # "commits",
-    # "compare",
-    # "created",
-    # "deleted",
-    # "forced",
-    # "head_commit",
-    # "pusher",
-    # "ref",
-    # "repository",
-    # "sender"
     ;;
   workflow_dispatch)
     printf "WORKFLOW_DISPATCH:\n"
@@ -58,6 +41,29 @@ case $GITHUB_EVENT_NAME in
     exit 1
     ;;
 esac
-
 before_exit
 exit 0
+
+# printf "event_file_attributes: %d\n" ${#event_file_attributes[@]}
+# printf "\t%s\n" ${event_file_attributes[@]}
+# printf "%s" $event_file_attributes_csv
+
+#printf "\n\nPUSH\n\n"
+# "after",
+# "base_ref",
+# "before",
+# "commits",
+# "compare",
+# "created",
+# "deleted",
+# "forced",
+# "head_commit",
+# "pusher",
+# "ref",
+# "repository",
+# "sender"
+
+# log PULL_REQUEST
+# AFTER -> NEW COMMIT
+# BEFORE -> OLD COMMIT
+# printf "GITHUB_REF=%s\n" "$GITHUB_REF"
