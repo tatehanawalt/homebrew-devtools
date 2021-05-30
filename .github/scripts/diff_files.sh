278d1a3c459a7cd0b7c32ddc19428cd581631fe7
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-30 15:33:07 -0500

    update

--- .github/scripts/diff_files.sh
+++ .github/scripts/diff_files.sh
@@ -50,7 +50,9 @@ diff_ext=()
 for f_path in ${diff_files[@]}; do
 
   filename="${f_path##*/}"
-  ext_name="$(echo $filename | sed s/.*\.//)"
+
+  ext_name="$(echo $filename | sed 's/.*\.//')"
+
   echo "filename: $filename"
   echo "ext_name: $ext_name"
 
