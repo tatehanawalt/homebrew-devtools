7712a16aba36944ad2aaa9928acb8f6eb5951b66
Tate Hanawalt <tate@tatehanawalt.com>
2021-06-18 03:26:42 -0500

    update

--- Formula/demozsh.rb
+++ Formula/demozsh.rb
@@ -33,7 +33,6 @@ class Demozsh < Formula
     lib.install 'demozsh.zsh'
 
     bin.install_symlink lib/"demozsh.zsh" => "demozsh"
-
     # bin.install 'demozsh.zsh' => 'demozsh'
     man1.install 'doc/man/demozsh.1'
   end
