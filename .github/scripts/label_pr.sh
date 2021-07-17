a70931bcb63a9c75e19cd3b0ff0d1ea69c534b1f
Tate Hanawalt <tate@tatehanawalt.com>
2021-07-17 15:32:26 -0500

    update

--- .github/scripts/label_pr.sh
+++ .github/scripts/label_pr.sh
@@ -1,52 +1,70 @@
 #!/bin/bash
 
+# Test Data
+#
+# export GITHUB_REPOSITORY=homebrew-devtools
+# export GITHUB_REPOSITORY_OWNER=tatehanawalt
+#
+#
+
 my_path=$0
 . "$(dirname $my_path)/helpers.sh"
 
 can_exec=0
-
-[ -z "$LABELS" ] && can_exec=1 && write_error "LABELS not set in label_pr"
-# && exit 1
-[ -z "$ID" ] && can_exec=1 && write_error "ID not set in label_pr"
-# && exit 1
-[ -z "$GITHUB_REPOSITORY" ] && can_exec=1 && write_error "GITHUB_REPOSITORY not set in label_pr"
-# && exit 1
-[ -z "$GITHUB_REPOSITORY_OWNER" ] && can_exec=1 && write_error "GITHUB_REPOSITORY_OWNER not set in label_pr"
-# && exit 1
-
-labels=($(echo -e $LABELS | tr ',' '\n'))
-
-printf "labels:\n"
-printf "\t%s\n" $labels
-
-json_data=$(printf "%s\n" "${labels[@]}" | jq -R . | jq -s .)
-printf "json_data: \n%s\n" "$json_data"
 
 args=( --url )
 args+=('repos/{owner}/{repo}/issues/{id}/labels')
 args+=(--method)
 args+=(POST)
 args+=(--auth)
-args+=(--json-body)
-args+=($(echo $json_data))
-args+=(--id)
-args+=($ID)
-args+=(--repo)
-args+=($(printf %s $GITHUB_REPOSITORY | sed 's/.*\///'))
-args+=(--owner)
-args+=($GITHUB_REPOSITORY_OWNER)
+
+if [ -z "$GITHUB_REPOSITORY" ];
+then
+  can_exec=1
+  write_error "GITHUB_REPOSITORY not set in label_pr"
+else
+  args+=(--repo)
+  args+=($(printf %s $GITHUB_REPOSITORY | sed 's/.*\///'))
+fi
+
+if [ -z "$GITHUB_REPOSITORY_OWNER" ]
+then
+  can_exec=1
+  write_error "GITHUB_REPOSITORY_OWNER not set in label_pr"
+else
+  args+=(--owner)
+  args+=($GITHUB_REPOSITORY_OWNER)
+fi
+
+if [ -z "$ID" ]
+then
+  can_exec=1
+  write_error "ID not set in label_pr"
+else
+  args+=(--id)
+  args+=($ID)
+fi
+
+if [ -z "$LABELS" ]
+then
+  can_exec=1
+  write_error "LABELS not set in label_pr"
+else
+  labels=($(echo -e $LABELS | tr ',' '\n'))
+  json_data=$(printf "%s\n" "${labels[@]}" | jq -R . | jq -s .)
+  # printf "json_data: \n%s\n" "$json_data"
+  args+=(--json-body)
+  args+=($(echo $json_data))
+fi
 
 printf "args:\n"
-printf "\t%s\n" $args
-
-printf "\n\nARGS:\n\n"
-printf "\t${args[@]}\n\n"
+printf "\t%s\n" ${args[@]}
 
 echo
 
 [ $can_exec -ne 0 ] && write_error "can_exec -ne 0..." && exit 1
 
-git_req -d $args[@]
+git_req -d ${args[@]}
 
 # results=($(git_req ${args[@]}))
 # printf "exit_code: \n\t%d\n" ${results[0]}
