445b06243f0ff88f64e5dc1f81f3fa2cf0c304ee
Tate Hanawalt <tate@tatehanawalt.com>
2021-07-19 14:28:07 -0400

    update

--- .github/scripts/diff_files.sh
+++ .github/scripts/diff_files.sh
@@ -1,94 +1,109 @@
 #!/bin/bash
 
-# DIFF_OUT=DIFF_FILES,DIFF_DIRS,DIFF_EXT,DIFF_ADD_LABEL_SET
-
-
 my_path=$0
 . "$(dirname $my_path)/helpers.sh"
 
-test_local
-
-can_exec=0
-add_label_set=()
-
 [ -z "$GITHUB_BASE_REF" ] && GITHUB_BASE_REF=main # original ref
 [ -z "$GITHUB_HEAD_REF" ] && GITHUB_HEAD_REF=main # current ref
+
 # Using the BASE branch
-[ -z "$GITHUB_BASE_REF" ]     && write_error "$(basename $0) GITHUB_BASE_REF length is 0... set GITHUB_BASE_REF=<branch_name> - line $LINENO" && can_exec=1
-[ -z "$GITHUB_WORKSPACE" ]    && write_error "$(basename $0) GITHUB_WORKSPACE length is 0 - line $LINENO" && can_exec=1
-[ ! -d "$GITHUB_WORKSPACE" ]  && write_error "$(basename $0) GITHUB_WORKSPACE is not a directory at GITHUB_WORKSPACE=$GITHUB_WORKSPACE - line $LINENO" && can_exec=1
-[ $can_exec -ne 0 ]           && write_error "can_exec -ne 0... can_exec=$can_exec - line $LINENO" && exit 1
+[ -z "$GITHUB_BASE_REF" ]    && write_error "$(basename $0) GITHUB_BASE_REF length is 0... set GITHUB_BASE_REF=<branch_name> - line $LINENO" && exit 1
+[ -z "$GITHUB_WORKSPACE" ]   && write_error "$(basename $0) GITHUB_WORKSPACE length is 0 - line $LINENO" && exit 1
+[ ! -d "$GITHUB_WORKSPACE" ] && write_error "$(basename $0) GITHUB_WORKSPACE is not a directory at GITHUB_WORKSPACE=$GITHUB_WORKSPACE - line $LINENO" && exit 1
+
+diff_files=()
+diff_ext=()
+diff_dirs=()
+diff_labels=()
 
 git fetch origin "$GITHUB_BASE_REF" &>/dev/null
 git branch "$GITHUB_BASE_REF" FETCH_HEAD &>/dev/null
 
 [ -z $(git branch --list "$GITHUB_BASE_REF") ] && write_error "$(basename $0) FETCH_HEAD for branch $GITHUB_BASE_REF - line $LINENO" && exit 1
 
-
-
-diff_files=($(git diff --name-only $GITHUB_BASE_REF | sed 's/[[:space:]]$//g' | sed 's/^[[:space:]]//g' | sort -u))
-
-
-echo $diff_files
-
-exit 0
-
-
-diff_files_csv=$(join_by , ${diff_files[@]})
-write_result_set "$diff_files_csv" diff_files
-
-diff_dirs=($(for_csv "$diff_files_csv" dirname | sort -u))
-write_result_set $(join_by , ${diff_dirs[@]}) diff_dirs
-
-diff_ext=()
-
-for f_path in ${diff_files[@]}; do
-  filename="${f_path##*/}"
-  ext_name="$(echo $filename | sed 's/^[^\.]*//')"
-  [ "$ext_name" = "$filename" ] && continue
-  ext_name="$(echo $ext_name | sed 's/^\.//')"
-  [ -z "$ext_name" ] && continue
-  diff_ext+=("$ext_name")
-done
-
-diff_ext=($(printf "%s\n" ${diff_ext[@]} | sort -u))
-write_result_set $(join_by , ${diff_ext[@]}) diff_ext
-
-for dir_path in ${diff_dirs[@]}; do
-  case $dir_path in
-    # Formula) add_label_set+=( brew );;
-    Formula) add_label_set+=(":beer:");;
-    .github/workflows | .github/scripts) add_label_set+=( action );;
-  esac
-done
-
-for fname in ${diff_files[@]}; do
-  case $fname in
-    # Formula/*.rb) add_label_set+=( formula brew $(basename $fname | sed 's/\..*//') );;
-    Formula/*.rb) add_label_set+=( formula ":beer:" $(basename $fname | sed 's/\..*//') );;
-  esac
-done
-
-for ext in ${diff_ext[@]}; do
-  case $ext in
-    c)    add_label_set+=(c);;
-    cpp)  add_label_set+=(cpp);;
-    json) add_label_set+=(json);;
-    md)   add_label_set+=( documentation markdown );;
-    py)   add_label_set+=(python);;
-    rb)   add_label_set+=(ruby);;
-    sh)   add_label_set+=(shell) ;;
-    svg)  printf "svg - leave this for now...\n" ;;
-    yaml) add_label_set+=(yaml) ;;
-    yml)
-      add_label_set+=(yaml)
-      echo "::warning file=$(basename $0),line=$LINENO::Encountered a yml file... $(basename $0):$LINENO"
+function on_label() {
+  [ -z "$1" ] && write_error "on_label \$1 length is zero - line $LINENO" && return 1
+  diff_labels+=("$1")
+  case "$1" in
+    "brew")
+      diff_labels+=(":beer:")
       ;;
-    *) write_error "$(basename $0) Unhandled Extension $ext - line $LINENO" ;;
+    "formula")
+      on_label "brew"
+      ;;
   esac
-done
+}
+function on_ext() {
+  [ -z "$1" ] && write_error "on_ext \$1 length is zero - line $LINENO" && return 1
+  diff_ext+=("$1")
+  case $1 in
+    c)
+      on_label "c"
+      ;;
+    cpp)
+      on_label "cpp"
+      ;;
+    json)
+      on_label "json"
+      ;;
+    md)
+      on_label "documentation"
+      on_label "markdown"
+      ;;
+    py)
+      on_label "python"
+      ;;
+    rb)
+      on_label "ruby"
+      ;;
+    sh)
+      on_label "shell"
+      ;;
+    svg)
+      printf "svg - leave this for now...\n"
+      ;;
+    yaml)
+      on_label "yaml"
+      ;;
+    yml)
+      on_label "yaml"
+      echo "::warning line=$LINENO::Encountered a yml extension... $1"
+      ;;
+    *)
+      write_error "Unhandled Extension '$1' - line $LINENO"
+      ;;
+  esac
+}
+function on_dir() {
+  [ -z "$1" ] && write_error "on_dir \$1 length is zero - line $LINENO" && return 1
+  diff_dirs+=($1)
+  case $1 in
+    Formula)
+      on_label "formula"
+      ;;
+    .github/workflows | .github/scripts)
+      on_label "action"
+      ;;
+  esac
+}
+function on_file_path() {
+  [ -z "$1" ] && write_error "on_file_path \$1 length is zero - line $LINENO" && return 1
+  diff_files+=($1)
+  on_dir "$(dirname $1)"
+  on_ext "$(echo ${1##*/} | sed 's/^[^\.]*//' | sed 's/^\.//')"
+  case $1 in
+    Formula/*.rb)
+      on_label "$(basename $1 | sed 's/\..*//')"
+      ;;
+  esac
+}
 
-diff_add_label_set=($(printf "%s\n" ${add_label_set[@]} | sed 's/.*\.//' | sort -u))
-write_result_set $(join_by , ${diff_add_label_set[@]}) diff_add_label_set
+for fpath in $(git diff --name-only $GITHUB_BASE_REF | sed 's/[[:space:]]$//g' | sed 's/^[[:space:]]//g');
+do on_file_path "$fpath"; done
+
+write_result_set "$(join_by , $(printf "%s\n" ${diff_files[@]} | sort -u))" "files"
+write_result_set "$(join_by , $(printf "%s\n" ${diff_ext[@]} | sort -u))" "extensions"
+write_result_set "$(join_by , $(printf "%s\n" ${diff_dirs[@]} | sort -u))" "directories"
+write_result_set "$(join_by , $(printf "%s\n" ${diff_labels[@]} | sort -u))" "labels"
 
 before_exit
