e594cf5b2507418bbfa0326cb0380d7b46d36738
Tate Hanawalt <tate@tatehanawalt.com>
2021-07-19 09:47:57 -0400

    update

--- .github/scripts/label_pr.sh
+++ .github/scripts/label_pr.sh
@@ -3,50 +3,46 @@
 my_path=$0
 . "$(dirname $my_path)/helpers.sh"
 
-can_exec=0
+local can_exec=0
 args=( --url )
 args+=('repos/{owner}/{repo}/issues/{id}/labels')
 args+=(--method)
 args+=(POST)
 args+=(--auth)
 
-if [ -z "$GITHUB_REPOSITORY" ];
-then
+if [ -z "$GITHUB_REPOSITORY" ]; then
   can_exec=1
   write_error "GITHUB_REPOSITORY not set in label_pr"
-else
-  args+=(--repo)
-  args+=($(printf %s $GITHUB_REPOSITORY | sed 's/.*\///'))
 fi
 
-if [ -z "$GITHUB_REPOSITORY_OWNER" ]
-then
+if [ -z "$GITHUB_REPOSITORY_OWNER" ]; then
   can_exec=1
   write_error "GITHUB_REPOSITORY_OWNER not set in label_pr"
-else
-  args+=(--owner)
-  args+=($GITHUB_REPOSITORY_OWNER)
 fi
 
-if [ -z "$ID" ]
-then
+if [ -z "$ID" ]; then
   can_exec=1
   write_error "ID not set in label_pr"
-else
-  args+=(--id)
-  args+=($ID)
 fi
 
-if [ -z "$LABELS" ]
-then
+if [ -z "$LABELS" ]; then
   can_exec=1
   write_error "LABELS not set in label_pr"
-else
-  labels=($(echo -e $LABELS | tr ',' '\n'))
-  json_data=$(printf "%s\n" "${labels[@]}" | jq -R . | jq -s -c .)
-  args+=(--json-body)
-  args+=($json_data)
 fi
+
+args+=(--id)
+args+=($ID)
+
+labels=($(echo -e $LABELS | tr ',' '\n'))
+json_data=$(printf "%s\n" "${labels[@]}" | jq -R . | jq -s -c .)
+args+=(--json-body)
+args+=($json_data)
+
+args+=(--owner)
+args+=($GITHUB_REPOSITORY_OWNER)
+
+args+=(--repo)
+args+=($(printf %s $GITHUB_REPOSITORY | sed 's/.*\///'))
 
 [ $can_exec -ne 0 ] && write_error "can_exec -ne 0..." && exit 1
 
