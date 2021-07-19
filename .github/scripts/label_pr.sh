63abb22aa16b10bd1f5244b4e7ca19a0340e4642
Tate Hanawalt <tate@tatehanawalt.com>
2021-07-19 14:54:49 -0400

    update

--- .github/scripts/label_pr.sh
+++ .github/scripts/label_pr.sh
@@ -4,7 +4,6 @@ my_path=$0
 . "$(dirname $my_path)/helpers.sh"
 
 add_labels=()
-
 function on_label() {
   [ -z "$1" ] && write_error "on_label \$1 length is zero - line $LINENO" && return 1
   add_labels+=("$1")
@@ -27,18 +26,13 @@ can_exec=0
 [ -z "$ID" ]                      && write_error "ID not set in label_pr - line $LINENO"                      && can_exec=1
 [ ${#add_labels[@]} -lt 1 ]       && write_error "add_labels count < 1. - line $LINENO"                       && can_exec=1
 
-json_data=$(printf "%s\n" "${add_labels[@]}" | jq -R . | jq -s -c .)
-
 args=(--url 'repos/{owner}/{repo}/issues/{id}/labels')
 args+=(--method POST)
 args+=(--id $ID)
 args+=(--auth)
-args+=(--json-body $json_data)
+args+=(--json-body $(printf "%s\n" "${add_labels[@]}" | jq -R . | jq -s -c .))
 args+=(--owner $GITHUB_REPOSITORY_OWNER)
 args+=(--repo "$(printf %s $GITHUB_REPOSITORY | sed 's/.*\///')")
-
-# DEBUG:
-# printf "args:\n"; printf "\t%s\n" ${args[@]}
 
 [ $can_exec -ne 0 ] && write_error "can_exec -ne 0 - line $LINENO" && exit 1
 git_req ${args[@]}
